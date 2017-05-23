98885374de6cb1af689604d3569ad5952cf2e653
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-05-23 09:36:50 +0200

    Remove lein-cloverage
    Cloverage's instrumentation interferes with metadata.

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-cloverage "1.0.9"]])
+  :plugins [])
