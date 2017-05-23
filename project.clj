518c41d2206b138a5b7c5e7a2fa371a2d3842064
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-05-23 09:38:43 +0200

    Use lein-release for release management

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [])
+  :plugins [[lein-release "1.0.5"]]
+  :lein-release {:deploy-via :clojars})
